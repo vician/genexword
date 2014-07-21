9e1aef5372cc03d74c77aa9674da989bcf05b1b1
Martin Vician <vician@ictmedia.cz>
2014-07-21 21:19:34 +0200

    Without param dict

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ genexword
 =========
 
 Generate exist word
+
+Small script to generete exists word.
+Primary used for DrawSomething, if you dont know current picture.
+
+This script genereate all unique combination of input chars with select length. These combination will compare with dictionary.
+And in last step script is using google translator for translation to another language.
+
+Usage:
+./genexword listchars N
+	listchars is list of availible char
+	N is lenght of finding word
---
+++ genexword.py
@@ -0,0 +1,42 @@
+#!/bin/python
+#
+# @author Martin Vician
+#
+
+import sys
+import itertools
+import enchant
+import numpy
+import collections
+from translate import Translator # https://github.com/terryyin/google-translate-python
+
+# params
+if len (sys.argv) != 3 :
+	print "Wrong arguments (",len(sys.argv),")!"
+	print "usage: "+sys.argv[0]+" chars length"
+	exit(1)
+
+# transalator
+translator = Translator(from_lang="en",to_lang="cs")
+# dictnary
+d = enchant.Dict("en_US")
+
+# get params
+chars = sys.argv[1]
+length = int(sys.argv[2])
+
+words=[] # array for unique values
+# all permutations
+i = 0
+for p in itertools.permutations(chars,length):
+	i += 1
+	print "\r",i,
+	word = ''.join(p)
+	if d.check(word):
+		if word not in words: # if is already finded?
+			words.append(word)
+			print "\r",word,
+			print "->",translator.translate(word),
+			print "\n",
+
+print "=> ",len(words)," exists words"
